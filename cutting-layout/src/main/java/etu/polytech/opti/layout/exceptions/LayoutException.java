3f1814b92d17f019790ae15b7d51d4aa3cbf2be0
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-03-19 23:36:21 +0100

    Base of the layout manager (WIP)

---
+++ cutting-layout/src/main/java/etu/polytech/opti/layout/exceptions/LayoutException.java
@@ -0,0 +1,10 @@
+package etu.polytech.opti.layout.exceptions;
+
+/**
+ * Created by Morgan on 19/03/2015.
+ */
+public abstract class LayoutException extends Exception {
+    public LayoutException(String message) {
+        super(message);
+    }
+}
