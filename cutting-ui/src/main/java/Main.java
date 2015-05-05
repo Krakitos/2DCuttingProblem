ee008650c7dd1aa93dca7a80ff62bc48ce98f084
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-05-05 15:19:07 +0200

    Removed stage reference in controller

--- cutting-ui/src/main/java/Main.java
+++ cutting-ui/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import etu.polytech.opti.controllers.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -19,9 +18,6 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("views/main.fxml"));
         Parent root = loader.load();
-
-        MainController controller = loader.getController();
-        controller.setStage(primaryStage);
 
         Scene scene = getScene(root);
         scene.getStylesheets().add("css/styles.css");
